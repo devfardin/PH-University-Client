89a418c66416c8268bd2d911b9d59f1aef94d1f5
devfardin <contactfardin22@gmail.com>
2025-01-10 16:54:40 +0600

    website side bar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -4,26 +4,29 @@ import { adminpaths } from '../../routes/admin.routes'
 import { Layout } from 'antd';
 import React from 'react';
 import logo from '../../assets/images/ph-logo.png'
+
 const { Sider } = Layout;
 interface SidebarProps {
-    collapsed: boolean; // Adjust the type as per your requirements
+    collapsed: boolean;
   }
+
 const Sidebar: React.FC<SidebarProps>  = ({collapsed}) => {
   return (
     <Sider trigger={null} collapsible collapsed={collapsed}>
     {/* Add you logo Here */}
 
     <div className="demo-logo-vertical">
-      {
-        !collapsed ? <h1 style={{fontSize: '25px', color: 'white', display: 'flex', justifyContent: 'center', marginTop: '15px', marginBottom: '15px'}}>PH Uni</h1> : <img className='hidden' src={logo} alt="Ph University Logo" />
-      }
+    {
+      !collapsed ? <h1 style={{fontSize: '25px', color: 'white', display: 'flex', justifyContent: 'center', marginTop: '15px', marginBottom: '15px'}}>PH Uni</h1> : <img style={{width: 50, margin:'auto', marginTop: 15, marginBottom: 20, }} src={logo} alt="ph University logo"/>
+    }
     </div>
     <Menu
       theme="dark"
       mode="inline"
-      defaultSelectedKeys={['dashboard']}
+      defaultSelectedKeys={['Dashboard']}
       items={sideBarItemsGenerator(adminpaths, 'admin')}
     />
+
   </Sider>
   )
 }

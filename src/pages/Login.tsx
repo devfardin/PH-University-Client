e22b09d3740dab7a244ddb8911b16644c175b4f0
devfardin <contactfardin22@gmail.com>
2025-01-12 19:08:05 +0600

    verify token form client

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form'
 import { useLoginMutation } from '../redux/features/auth/authApi';
 import { useAppDispatch } from '../redux/hooks';
 import { setUser } from '../redux/features/auth/authSlice';
+import { verifyToken } from '../utils/verifyToken';
 const Login = () => {
   const dispatch = useAppDispatch()
   const {register, handleSubmit} = useForm(
@@ -20,13 +21,12 @@ const Login = () => {
   const [login, {error}] = useLoginMutation();
   const onSubmit = async(data:ILoginForm) =>{
    const res = await login(data).unwrap();
-   dispatch(setUser({user: {}, token: res.data.accessToken}))
-   
+   const user = verifyToken(res.data.accessToken);
+   dispatch(setUser({user: user, token: res.data.accessToken}))
   };
 
-  
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} style={{display:'flex', alignItems:'center', justifyContent: 'center', flexDirection: 'column', gap: 20, height: '100vh'}}>
       <div>
         <label htmlFor='id'>ID:</label>
         <input type="text" id='id' {...register("id")} />

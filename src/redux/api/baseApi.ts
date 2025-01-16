a31751f1672abc2b0fb9a6a310f9d465807330bf
devfardin <contactfardin22@gmail.com>
2025-01-16 15:41:09 +0600

    update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -14,9 +14,6 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
 const result = await baseQuery(args, api, extraOptions);
-console.log(result);
-
-
 }
 
 export const baseApi = createApi({
